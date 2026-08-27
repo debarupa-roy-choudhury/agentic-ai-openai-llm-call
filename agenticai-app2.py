bc113af22c8253c62e7a01b847088f10bb891bb9
Debarupa <royc.debarupa@gmail.com>
2026-08-27 10:48:26 +0200

    second commit: use openai sdk

---
+++ agenticai-app2.py
@@ -0,0 +1,14 @@
+
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input="Tell me about recent advancements in agentic ai field",
+)
+
+print(response.output_text)
